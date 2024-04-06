9948c29a6833564976680331b5c2651633fdd3ba
Stevenson <michels@berea.edu>
2024-04-06 09:27:12 -0400

    Added new configurations of admin

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "rating")
+    list_display = ("title", "author")
+
+admin.site.register(Book, BookAdmin)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -8,18 +8,14 @@ from django.utils.text import slugify
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(5)]
-    )
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True) 
+    slug = models.SlugField(default="", blank=True, editable=False, null=False, db_index=True) 
     
     def get_absolute_url(self):
         return reverse("book_detail", args=[self.slug])
-    
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+
     
     def __str__(self):
         return f"{self.title} ({self.rating})"
